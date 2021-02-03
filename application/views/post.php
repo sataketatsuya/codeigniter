3a0632a0c470d02604420a5cb8c7f1844967286c
sataketatsuya1121 <tatsuyaenginner@gmail.com>
2021-02-03 09:24:10 +0900

    ヘッダー修正

--- application/views/post.php
+++ application/views/post.php
@@ -31,11 +31,6 @@ function e(string $text): string
 <body>
     <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
         <h5 class="my-0 mr-md-auto font-weight-normal">ひと言掲示板</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="/login/index">ログイン</a>
-            <a class="p-2 text-dark" href="/register/index">新規登録</a>
-            <a class="p-2 text-dark" href="#">ログアウト</a>
-        </nav>
     </div>
 
     <div class="container">
